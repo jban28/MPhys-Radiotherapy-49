00459ff373e490b3ee7c688a029b05e38e851785
jban28 <jd.bannister28@gmail.com>
2021-11-11 13:42:21 +0000

    1.42pm 11/11

--- File-select.py
+++ File-select.py
@@ -1,23 +1,32 @@
 import os
+import pydicom
 
 project_folder = "/mnt/c/Users/James/Documents/MPhys-Project/"
 images = []
-chum_list = []
-chus_list = []
-hgj_list = []
-hmr_list = []
+# chum_list = []
+# chus_list = []
+# hgj_list = []
+# hmr_list = []
 
-paths = os.listdir(project_folder+"Sorted_data")
+path_list = os.listdir(project_folder+"Sorted_data")
 
-for file in paths:
-  if file[:7] == "HN-CHUM":
-    chum_list.append(file)
-  elif file[:7] == "HN-CHUS":
-    chus_list.append(file)
-  elif file[:6] == "HN-HGJ":
-    hgj_list.append(file)
-  elif file[:6] == "HN-HMR":
-    hmr_list.append(file)
+for path in path_list:
+  dicom = pydicom.dcmread(project_folder+"Sorted_data/"+path+"/"+os.listdir(project_folder+"Sorted_data/"+path)[0])
+  print(dicom.Modality)
+  # if (dicom.Modality() != "CT") and (dicom.Modality() != "RTSTRUCT"):
+  #   path_list.remove(path)
 
-for chum in chum_list:
-  
+print(path_list)
+
+# for file in paths:
+#   if file[:7] == "HN-CHUM":
+#     chum_list.append(file)
+#   elif file[:7] == "HN-CHUS":
+#     chus_list.append(file)
+#   elif file[:6] == "HN-HGJ":
+#     hgj_list.append(file)
+#   elif file[:6] == "HN-HMR":
+#     hmr_list.append(file)
+
+# for chum in chum_list:
+
