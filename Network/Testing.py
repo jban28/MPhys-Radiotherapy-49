7efc887e2877a17bc81a6689ac99720989a73482
jban28 <jd.bannister28@gmail.com>
2022-04-07 14:02:15 +0000

    Testing.py cleaned up

--- Network/Testing.py
+++ Network/Testing.py
@@ -30,16 +30,19 @@ date = "2022_03_29_15_46_01"
 
 # model = CNN().to(device)
 model = ResNet.generate_model(10).to(device)
-model.load_state_dict(torch.load(f'models/{date}'))
 #===============================================================================
 
-# target_layers = [model.layer4[-1]]
-
+# Open test image data
 open_file = open(f"test_data/{date}.pkl", "rb")
 test_outcomes = pickle.load(open_file)
 open_file.close()
 
-#print(test_outcomes)
+# Load model state for testing
+model.load_state_dict(torch.load(f'models/{date}'))
+
+# Create file for test results if one does not exist
+if not os.path.exists(f"test_runs/{date}"):
+    os.mkdir(f"test_runs/{date}")
 
 # Find the original metadata for the patients 
 # Open the metadata.csv file, convert to an array, and remove column headers
@@ -53,10 +56,10 @@ metadata[0][0] + ".nii")
 image = sitk.GetArrayFromImage(image_sitk)
 image_dimension = image.shape[0]
 
+# Create dataset and dataloader
 test_data = ImageDataset(test_outcomes, project_folder + "/" + subfolder + 
 "/Images/", rotate_augment=False, scale_augment=False, flip_augment=False, 
 shift_augment=False, cube_size=image_dimension)
-
 test_dataloader = DataLoader(test_data, 1, shuffle=False)
 
 # Testing
@@ -64,12 +67,8 @@ print("Testing")
 test_predictions, test_targets = test_loop(test_dataloader, model, device, 
 image_dimension)
 
+# Define test results
 test_results = Results(test_predictions,test_targets)
-
-# check to see if csv file exists and append test results
-if not os.path.exists("test_runs"):
-    os.mkdir("test_runs")
-exists = os.path.isfile('Results.csv')
 accuracy = test_results.accuracy()
 sensitivity = test_results.sensitivity
 precision = test_results.precision
@@ -80,104 +79,25 @@ fn = test_results.fn
 fp = test_results.fp
 specificity = test_results.specificity
 G_mean = test_results.G_mean
-if exists:
+
+# Append test results to csv
+if os.path.isfile('Results.csv'):
     # Append row
     with open('test_runs/Results.csv', 'a', newline='') as csvfile:
         filewriter = csv.writer(csvfile, delimiter=',')
-        filewriter.writerow([date, accuracy, sensitivity, precision, F1_measure, tp, tn, fp, fn, 
-                            specificity, G_mean])
+        filewriter.writerow([date, accuracy, sensitivity, precision, F1_measure,
+         tp, tn, fp, fn, specificity, G_mean])
 else:
     # Create and then add row
     with open('test_runs/Results.csv', 'w', newline='') as csvfile:
         filewriter = csv.writer(csvfile, delimiter=',')
-        filewriter.writerow(['Date/time', 'Accuracy', 'Sensitivity', 'Precision', 'F1 measure',
-                            'True positive', 'True negative', 'False positive', 
-                            'False negative', 'Specificity', 'G mean'])
-        filewriter.writerow([date, accuracy, sensitivity, precision, F1_measure, tp, tn, fp, fn, 
-                            specificity, G_mean])
+        filewriter.writerow(['Date/time', 'Accuracy', 'Sensitivity','Precision',
+        'F1 measure', 'True positive', 'True negative', 'False positive', 
+        'False negative', 'Specificity', 'G mean'])
+        filewriter.writerow([date, accuracy, sensitivity, precision, F1_measure,
+        tp, tn, fp, fn, specificity, G_mean])
 
-
-
-
-
-# GradCAM
-# layer_names = ['conv1', 'layer1', 'layer2', 'layer3', 'layer4', 'conv2']
-# for i in range(6):
-#     layers = layer_names[i]
-#     print(layers)
-#     model = medcam.inject(model, output_dir="medcam_test", 
-#         save_maps=True, layer=layers, replace=True)
-#     #print(medcam.get_layers(model))
-#     model.eval()
-#     image, label, pid = next(iter(test_dataloader))
-#     filename = pid[0][0]
-#     image = image[None].to(device, torch.float)
-#     attn = model(image)
-#     attn = np.squeeze(attn.cpu().numpy())
-#     img = np.squeeze(image.cpu().numpy())
-#     print(img.shape, attn.shape)
-#     slice_num = 102
-#     fig, ax = plt.subplots(1,1, figsize=(10,10))
-#     im = img[..., slice_num]
-#     attn = attn[..., slice_num]
-#     print(pid)
-#     print(attn.max(), attn.min())
-#     ax.imshow(im, cmap='gray')
-#     ax.imshow(attn, cmap='jet', alpha=0.5)
-#     fig.savefig('./GradCAM_'+ layer_names[i] +'.png')
-
-# for looking at the convolutional layers conv1 and conv2
-
-# for i in range(2):
-#     layer = f'conv{i+1}'
-#     print(layer)
-#     model = medcam.inject(model, output_dir="medcam_test", 
-#         save_maps=True, layer=layer, replace=True)
-#     #print(medcam.get_layers(model))
-#     model.eval()
-#     image, label, pid = next(iter(test_dataloader))
-#     filename = pid[0][0]
-#     image = image[None].to(device, torch.float)
-#     attn = model(image)
-
-#     attn = np.squeeze(attn.cpu().numpy())
-#     img = np.squeeze(image.cpu().numpy())
-#     print(img.shape, attn.shape)
-#     slice_num = 102
-#     fig, ax = plt.subplots(1,1, figsize=(10,10))
-#     im = img[..., slice_num]
-#     attn = attn[..., slice_num]
-#     print(pid)
-#     print(attn.max(), attn.min())
-#     ax.imshow(im, cmap='gray')
-#     ax.imshow(attn, cmap='jet', alpha=0.5)
-#     fig.savefig(f'./GradCAM_conv{i+1}.png')
-
-# layer = 'layer1'
-# #print(layer)
-# model = medcam.inject(model, output_dir="medcam_test", 
-#     save_maps=True, layer=layer, replace=True)
-# print(medcam.get_layers(model))
-# model.eval()
-# image, label, pid = next(iter(test_dataloader))
-# filename = pid[0][0]
-# image = image[None].to(device, torch.float)
-# attn = model(image)
-
-# attn = np.squeeze(attn.cpu().numpy())
-# img = np.squeeze(image.cpu().numpy())
-# print(img.shape, attn.shape)
-# slice_num = 102
-# fig, ax = plt.subplots(1,1, figsize=(10,10))
-# im = img[..., slice_num]
-# attn = attn[..., slice_num]
-# print(pid)
-# print(attn.max(), attn.min())
-# ax.imshow(im, cmap='gray')
-# ax.imshow(attn, cmap='jet', alpha=0.5)
-# fig.savefig('./GradCAM_layer1.png')
-if not os.path.exists(f"test_runs/{date}"):
-    os.mkdir(f"test_runs/{date}")
+# Save medcam images for each layer
 layer_names = ['conv1', 'layer1', 'layer2', 'layer3', 'layer4', 'conv2']
 for layer in layer_names:
     model = medcam.inject(model, output_dir="medcam_test", 
